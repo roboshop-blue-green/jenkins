d5499181885dc66e9563bf0a1c6d5d66fde5f3d5
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-23 15:05:37 +0530

    Update jenkins/

--- vars/common.groovy
+++ vars/common.groovy
@@ -3,7 +3,7 @@ def prepareArtifacts() {
     if(COMPONENT == "frontend") {
       sh '''
         ls -l
-        APP_VERSION=`cat VERSION.md | grep ^# | head -1 | sed -e 's/#//'`
+        APP_VERSION=`cat VERSION.md | grep ^# | head -1 | sed -e 's/#//' -e 's/ //g'`
         cd static
         zip -r ../${COMPONENT}-${APP_VERSION}.zip * 
       '''
@@ -14,7 +14,7 @@ def prepareArtifacts() {
 def publishArtifacts() {
   stage('Publish Artifacts') {
     sh '''
-        APP_VERSION=`cat VERSION.md | grep ^# | head -1 | sed -e 's/#//'`
+        APP_VERSION=`cat VERSION.md | grep ^# | head -1 | sed -e 's/#//' -e 's/ //g'`
         curl -f -v -u admin:admin --upload-file ${COMPONENT}-${APP_VERSION}.zip  http://172.31.8.57:8081/repository/${COMPONENT}/${COMPONENT}-${APP_VERSION}.zip
       '''
   }
