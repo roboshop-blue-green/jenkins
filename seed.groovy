ff520c861ebe285785c267c64ab05840565492c4
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-23 14:27:38 +0530

    Update jenkins/

---
+++ seed.groovy
@@ -0,0 +1,25 @@
+folder('CI-Pipelines') {
+  displayName('CI-Pipelines')
+  description('All CI Pipelines')
+}
+
+pipelineJob('CI-Pipelines/frontend') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/roboshop-blue-green/frontend')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
