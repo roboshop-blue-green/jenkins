56717a4737961ecc5630a2145b232079b2707c84
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-24 12:31:14 +0530

    Update jenkins/

--- vars/common.groovy
+++ vars/common.groovy
@@ -2,10 +2,29 @@ def prepareArtifacts() {
   stage('Prepare Artifacts') {
     if(COMPONENT == "frontend") {
       sh '''
-        ls -l
         APP_VERSION=`cat VERSION.md | grep ^# | head -1 | sed -e 's/#//' -e 's/ //g'`
         cd static
         zip -r ../${COMPONENT}-${APP_VERSION}.zip * 
+      '''
+    }
+    if(APP_TYPE == "nodejs") {
+      sh '''
+        APP_VERSION=`cat VERSION.md | grep ^# | head -1 | sed -e 's/#//' -e 's/ //g'`
+        npm install
+        zip -r ${COMPONENT}-${APP_VERSION}.zip node_modules server.js
+      '''
+    }
+    if(APP_TYPE == "maven") {
+      sh '''
+        APP_VERSION=`cat VERSION.md | grep ^# | head -1 | sed -e 's/#//' -e 's/ //g'`
+        mvn clean package && mv target/shipping-1.0.jar shipping.jar 
+        zip -r ${COMPONENT}-${APP_VERSION}.zip shipping.jar
+      '''
+    }
+    if(APP_TYPE == "python") {
+      sh '''
+        APP_VERSION=`cat VERSION.md | grep ^# | head -1 | sed -e 's/#//' -e 's/ //g'` 
+        zip -r ${COMPONENT}-${APP_VERSION}.zip *.py *.ini requirements.txt 
       '''
     }
   }
